9fd48f0e7efe5c4744872bf7c231f556fea6bfa1
naszi <naszi75@gmail.com>
2024-03-16 00:02:05 +0100

    values, types, variable and switch two values

---
+++ main.py
@@ -0,0 +1,50 @@
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+print("a=")
+print(a)
+print("b=")
+print(b)
+
+
+# String
+# print('Hello')
+# print("World")
+# print("""
+# Hello
+# World
+# """)
+
+# Integer
+# print(12)
+# print(12 + 10)
+# print(1,000,000) #It's falls! out -> 1 0 0
+# print(1_000_000) # It's works fine! -> 1000000
+
+# Float
+# print(3.14159)
+
+# Boolean - True or False
+# print(True)
+# print(False)
+
+# type() - function
+# print(type(120))
+# print(type("Hello"))
+# print(type(True))
+
+# variable
+# edy = 123456747
+# message = "I am learning Python"
+# n = 17
+# pi = 3.14159
+# print(edy)
+#
+# name = "Szabolcs"
+# print(name)
+# name = "Gabor"
+# print(name)
+# name = 1
+# print(name)
